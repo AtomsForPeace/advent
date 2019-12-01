70322c175f4abe47f67c7c4eeb08eb32c4c297a0
Adam Bannister <adam.p.bannister@gmail.com>
2019-12-01 09:45:59 +0100

    day one complete in python

---
+++ day1.py
@@ -0,0 +1,41 @@
+import math
+
+
+# Part 1
+
+
+def function_part_1(mass: int) -> int:
+    return math.floor(mass / 3) - 2
+
+
+def test_function_part_1():
+    tests = [(12, 2), (14, 2), (1969, 654), (100756, 33583)]
+    for mass, fuel in tests:
+        assert function_part_1(mass) == fuel
+
+
+# Part 2
+
+
+def function_part_2(mass: int, current_level: int) -> int:
+    fuel = function_part_1(mass)
+    if fuel <= 0:
+        return current_level
+    return function_part_2(mass=fuel, current_level=current_level + fuel)
+
+
+def test_function_part_2():
+    tests = [(14, 2), (1969, 966), (100756, 50346)]
+    for mass, fuel in tests:
+        assert function_part_2(mass, 0) == fuel
+
+
+if __name__ == '__main__':
+    with open('day1.txt') as f:
+        lines = [int(line) for line in f.readlines()]
+
+    print('Part 1')
+    print(sum(map(function_part_1, lines)))
+
+    print('Part 2')
+    print(sum([function_part_2(line, 0) for line in lines]))
